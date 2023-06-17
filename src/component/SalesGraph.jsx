01602bb0a9a402fb00274b78df913fadf913ce85
Muhammad Fiqri <muhfikri6565@gmail.com>
2023-06-18 03:33:29 +0700

    pie-graph is done!

--- src/component/SalesGraph.jsx
+++ src/component/SalesGraph.jsx
@@ -1,4 +1,5 @@
 import "../css/SalesGraph.scss";
+import PieComponent from "./Piechart";
 
 function SalesGraph()
 {
@@ -12,6 +13,24 @@ function SalesGraph()
                 <input type="date" name="start" />
                 <p>Hingga</p>
                 <input type="date" name="end"/>
+            </div>
+            <PieComponent/>
+            <div className="data-info">
+                <div className="data-1">
+                    <h3>60%</h3>
+                    <p>Tour</p>
+                    <div className="color-chart"></div>
+                </div>
+                <div className="data-2">
+                    <h3>20%</h3>
+                    <p>Tour</p>
+                    <div className="color-chart"></div>
+                </div>
+                <div className="data-3">
+                    <h3>30%</h3>
+                    <p>Tour</p>
+                    <div className="color-chart"></div>
+                </div>
             </div>
         </div>
     );
