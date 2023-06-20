8aa993f885606623ba2e2f3b1b329f1ffb4e3b65
Muhammad Fiqri <muhfikri6565@gmail.com>
2023-06-21 02:11:22 +0700

    all front end done entering 0.0.1

---
+++ src/component/ChooseProduct.jsx
@@ -0,0 +1,128 @@
+import "../css/ChooseProduct.scss";
+
+function ChooseProduct() {
+    return(
+        <div className="ChooseProduct-wrapper">
+            <div className="form-header">
+                <h2>Produk</h2>
+                <p>Silakan pilih produk yang diinginkan</p>
+            </div>
+            <div className="form-body">
+                <div className="product-wrapper">
+                    <img src="./assets/produk.png" alt="" className="product-img" />
+                    <div className="prod-header">
+                        <p className="product-name">Nama Produk: Havelock</p>
+                        <p className="product-price">Harga Produk: Rp. 9,999,999</p>
+                    </div>
+                    <div className="prod-desc">
+                        <p>Deksripsi: </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            </p>
+                    </div>
+                </div>
+                <div className="product-wrapper">
+                    <img src="./assets/produk.png" alt="" className="product-img" />
+                    <div className="prod-header">
+                        <p className="product-name">Nama Produk: Havelock</p>
+                        <p className="product-price">Harga Produk: Rp. 9,999,999</p>
+                    </div>
+                    <div className="prod-desc">
+                        <p>Deksripsi: </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            </p>
+                    </div>
+                </div>
+                <div className="product-wrapper">
+                    <img src="./assets/produk.png" alt="" className="product-img" />
+                    <div className="prod-header">
+                        <p className="product-name">Nama Produk: Havelock</p>
+                        <p className="product-price">Harga Produk: Rp. 9,999,999</p>
+                    </div>
+                    <div className="prod-desc">
+                        <p>Deksripsi: </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            </p>
+                    </div>
+                </div>
+                <div className="product-wrapper">
+                    <img src="./assets/produk.png" alt="" className="product-img" />
+                    <div className="prod-header">
+                        <p className="product-name">Nama Produk: Havelock</p>
+                        <p className="product-price">Harga Produk: Rp. 9,999,999</p>
+                    </div>
+                    <div className="prod-desc">
+                        <p>Deksripsi: </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            </p>
+                    </div>
+                </div>
+                <div className="product-wrapper">
+                    <img src="./assets/produk.png" alt="" className="product-img" />
+                    <div className="prod-header">
+                        <p className="product-name">Nama Produk: Havelock</p>
+                        <p className="product-price">Harga Produk: Rp. 9,999,999</p>
+                    </div>
+                    <div className="prod-desc">
+                        <p>Deksripsi: </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            </p>
+                    </div>
+                </div>
+                <div className="product-wrapper">
+                    <img src="./assets/produk.png" alt="" className="product-img" />
+                    <div className="prod-header">
+                        <p className="product-name">Nama Produk: Havelock</p>
+                        <p className="product-price">Harga Produk: Rp. 9,999,999</p>
+                    </div>
+                    <div className="prod-desc">
+                        <p>Deksripsi: </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            </p>
+                    </div>
+                </div>
+                <div className="product-wrapper">
+                    <img src="./assets/produk.png" alt="" className="product-img" />
+                    <div className="prod-header">
+                        <p className="product-name">Nama Produk: Havelock</p>
+                        <p className="product-price">Harga Produk: Rp. 9,999,999</p>
+                    </div>
+                    <div className="prod-desc">
+                        <p>Deksripsi: </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            </p>
+                    </div>
+                </div>
+                <div className="product-wrapper">
+                    <img src="./assets/produk.png" alt="" className="product-img" />
+                    <div className="prod-header">
+                        <p className="product-name">Nama Produk: Havelock</p>
+                        <p className="product-price">Harga Produk: Rp. 9,999,999</p>
+                    </div>
+                    <div className="prod-desc">
+                        <p>Deksripsi: </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            </p>
+                    </div>
+                </div>
+                <div className="product-wrapper">
+                    <img src="./assets/produk.png" alt="" className="product-img" />
+                    <div className="prod-header">
+                        <p className="product-name">Nama Produk: Havelock</p>
+                        <p className="product-price">Harga Produk: Rp. 9,999,999</p>
+                    </div>
+                    <div className="prod-desc">
+                        <p>Deksripsi: </p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    </div>
+                </div>
+                
+            </div>
+            <div className="form-footer">
+                <button id="cancel">Cancel</button>
+                <button id="save">Save</button>
+            </div>
+        </div>
+    );
+}
+
+export default ChooseProduct;
---
+++ src/component/ReportSalesResume.jsx
@@ -0,0 +1,109 @@
+import "../css/ReportSalesResume.scss";
+import Table from 'rc-table';
+
+function ReportSalesResume() 
+{
+    const columns = [
+        {
+            title: 'NO',
+            dataIndex: 'no',
+            key: 'no',
+            width: 100,
+            align: 'center',
+          },
+        {
+          title: 'TGL PEMESANAN',
+          dataIndex: 'tgl_pemesanan',
+          key: 'tgl_pemesanan',
+          width: 100,
+          align: 'center',
+        },
+        {
+          title: 'ID PEMESANAN',
+          dataIndex: 'id_pemesanan',
+          key: 'id_pemesanan',
+          width: 100,
+          align: 'center',
+        },
+        {
+          title: 'NAMA PEMESAN',
+          dataIndex: 'nama_pemesan',
+          key: 'nama_pemesan',
+          width: 200,
+          align: 'center',
+        },
+        {
+          title: 'SUBTOTAL PENJUALAN',
+          dataIndex: 'subtotal_penjualan',
+          key: 'subtotal_penjualan',
+          width: 100,
+          align: 'center',
+        },
+        {
+            title: 'POTONGAN',
+            dataIndex: 'potongan',
+            key: 'potongan',
+            width: 100,
+            align: 'center',
+        },
+        {
+            title: 'TOTAL PENJUALAN',
+            dataIndex: 'total_penjualan',
+            key: 'total_penjualan',
+            width: 100,
+            align: 'center',
+        },
+        {
+            title: 'PEMBAYARAN',
+            dataIndex: 'pembayaran',
+            key: 'pembayaran',
+            width: 100,
+            align: 'center',
+        },
+        {
+            title: 'PIUTANG PENJUALAN',
+            dataIndex: 'piutang_penjual',
+            key: 'piutang_penjualan',
+            width: 100,
+            align: 'center',
+        },
+    ];
+
+    const config_expand = {
+            expandRowByClick : true,
+        }
+
+    const data = [
+        { no: "1", tgl_pemesanan: "11/02/2023", id_pemesanan: "08979898999", nama_pemesan: "Graha skdha", subtotal_penjualan: "Rp9.999.999", potongan: "Rp9.999.999", total_penjualan: "Rp9.999.999", pembayaran : "Rp9.999.999", piutang_penjual : "Rp9.999.999", key: '1' },
+        { no: "2", tgl_pemesanan: "11/02/2023", id_pemesanan: "08979898999", nama_pemesan: "Graha skdha", subtotal_penjualan: "Rp9.999.999", potongan: "Rp9.999.999", total_penjualan: "Rp9.999.999", pembayaran : "Rp9.999.999", piutang_penjual : "Rp9.999.999", key: '2' },
+    ];
+    return(
+        <div className="ReportSalesResume-wrapper">
+            <h2>Sales Report Resume</h2>
+            <div className="search-bar">
+                <input type="text" name="search_input" placeholder="Search"/>
+                <img src="./assets/search-icon.svg"></img>
+                <button>Cari</button>
+            </div>
+            <div className="show-entries">
+                <p>Show</p>
+                <select>
+                    <option>2</option>
+                </select>
+                <p>Entries</p>
+            </div>
+            <div className="interval">
+                <p>Mulai dari</p>
+                <input type="date" name="start" />
+                <p>Hingga</p>
+                <input type="date" name="end"/>
+            </div>
+            <div className="export-button">
+                <button>Export</button>
+            </div>
+            <Table columns={columns} data={data} expandable={config_expand} tableLayout="fixed"/>
+        </div>
+    );
+}
+
+export default ReportSalesResume;
